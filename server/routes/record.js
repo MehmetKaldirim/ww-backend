1dad86a6fbb9e5b7dfc6deac06473d008d26817d
Mehmet <mathdenizi@gmail.com>
2024-11-19 03:27:23 +0100

    usage of passport for Routes Guearding

--- server/routes/record.js
+++ server/routes/record.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const passport = require("passport");
 const controller = require("../controllers/recordController");
+
 const router = express.Router(); //create module router here
-router.get("/", controller.getAll);
+
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  controller.getAll
+);
 router.post("/", controller.newRecord);
 router.get("/:id", controller.getById);
 router.put("/:id", controller.updRecord);
 router.delete("/:id", controller.deleteRecord);
+
 module.exports = router;
